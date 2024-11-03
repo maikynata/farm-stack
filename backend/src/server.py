65209fb1ea548cfe157ec68750748bac1003c852
maikynata <maikynata25@gmail.com>
2024-11-03 16:25:01 -0300

    creating the fastapi

--- backend/src/server.py
+++ backend/src/server.py
@@ -14,3 +14,22 @@ from dal import ToDoDAL, ListSummary, ToDoList
 COLLECTION_NAME = "todo_lists"
 MONGODB_URI = os.environ["MONGODB_URI"]
 DEBUG = os.environ.get("DEBUG", "").strip().lower() in {"1", "true", "on", "yes"}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    client = AsyncIOMotorClient(MONGODB_URI)
+    database = client.get_default_database()
+
+    pong = await database.command("ping")
+    if int(pong["ok"]) != 1:
+        raise Exception("Cluster connection is not okay")
+    
+    todo_lists = database.get_collection(COLLECTION_NAME)
+    app.todo_dal = ToDoDAL(todo_lists)
+
+    # Yield back to FastAPI Application:
+    yield
+
+    client.close()
+
+app = FastAPI(lifespan=lifespan, debug=DEBUG)
